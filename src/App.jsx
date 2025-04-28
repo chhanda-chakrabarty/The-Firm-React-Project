ed2cb4183565ab0bea94483296770331728c30bd
chhanda-chakrabarty <chhandachakrabarty7@gmail.com>
2025-04-29 00:29:25 +0600

    updated theFirm

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router";
-import Banner from "./components/Hero Section/Banner";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Root from "./Root";
+import Cases from "./pages/Cases";
+import CaseDetails from "./pages/CaseDetails";
+import SolutionPage from "./pages/SolutionPage";
+import BlogPage from "./pages/BlogPage";
+const root = document.getElementById("root");
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Banner />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cases" element={<Cases />} />
+          <Route path="/casedetails" element={<CaseDetails />} />
+          <Route path="/solution" element={<SolutionPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/Cases.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CasesBanner from "../components/CasesBanner";
+// import CasesCategory from "../components/casesCategory";
+import CasesCategory from "../components/CasesCategory";
+import Testimonial from "../components/Testimonial";
+
+const Cases = () => {
+  return (
+    <div className="">
+      <CasesBanner />
+      <CasesCategory />
+      <Testimonial />
+    </div>
+  );
+};
+
+export default Cases;
